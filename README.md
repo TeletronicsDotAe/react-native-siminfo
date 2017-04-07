f39c8a82ebf8165fd5534a3453f81f4449413fdb
Tommy Pedersen <tpedersen@gmail.com>
2017-04-07 15:18:25 +0200

    Initial commit of skeleton code

---
+++ README.md
@@ -0,0 +1,23 @@
+# react-native-siminfo
+Get sim card info from Android apps written in React Native
+
+## Installation 
+
+## Supported Platforms
+
+- Android
+
+## Usage
+
+## Android setup
+
+### Requirements
+
+Install `Extras` / `Android Support Library` from Android SDK.
+
+### Permissions
+
+Requires permission `READ_PHONE_STATE` in Android manifest file
+
+
+
---
+++ sim.js
@@ -0,0 +1,18 @@
+var RNSimInfo = require('react-native').NativeModules.RNSimInfo;
+
+module.exports = {
+
+    getSimInfo: function () {
+	return RNSimInfo;
+    },  
+    getTelephoneNumber: function () {
+	return RNSimInfo.telephoneNumber;
+    },
+    getCarrierName: function () {
+	return RNSimInfo.carrierName;
+    },
+    getCountryCode: function () {
+	return RNSimInfo.countryCode;
+    }
+
+};
